5f9e5de7ab0c1df59a5b3b86037af62fec94aa71
vig3995 <vig3995@thi.de>
2026-04-17 17:43:07 +0200

    Add camera optical frames to sensor_kit.xacro

---
+++ id4_sensor_kit_launch/launch/sensor_kit_tf.launch.py
@@ -0,0 +1,23 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    xacro_file = os.path.join(
+        get_package_share_directory('id4_sensor_kit_description'),
+        'urdf',
+        'sensor_kit.urdf.xacro'
+    )
+    robot_description = xacro.process_file(xacro_file).toxml()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        )
+    ])
